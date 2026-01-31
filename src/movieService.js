d57c8e07fa30bac85bbe5dbad76bf6bbc20d848a
Mateo Romero <materomero04@gmail.com>
2026-01-31 00:15:25 -0300

    Fixed typo in movies.json, Enriched movies list with data fetched from the omdb API

--- src/movieService.js
+++ src/movieService.js
@@ -1,5 +1,8 @@
 import fs from "fs";
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const movies = JSON.parse(fs.readFileSync("data/movies.json"));
 
@@ -8,8 +11,32 @@ export function getMovies() {
 }
 
 console.log("Movie service loaded with", movies.length, "movies");
-console.log(movies);
-
-
 axios.defaults.baseURL = "http://www.omdbapi.com/";
+async function enrichMoviesWithApiData() {
+  const enrichedMovies = await Promise.all(
+    movies.map(async (movie) => {
+      try {
+        const response = await axios.get(
+          `?apikey=${process.env.OMDB_API_KEY}&i=${movie.imdbID}`,
+        );
+        return {
+          ...movie,
+          ...response.data,
+        };
+      } catch (error) {
+        console.error(
+          `Failed to fetch data for ${movie.imbdID}:`,
+          error.message,
+        );
+        return movie;
+      }
+    }),
+  );
+  return enrichedMovies;
+}
+
+enrichMoviesWithApiData().then((enrichedMovies) => {
+  console.log("\nEnriched movies with API data:");
+  console.log(enrichedMovies);
+});
 
